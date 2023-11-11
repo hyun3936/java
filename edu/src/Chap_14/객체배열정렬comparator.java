047afb299ffa12fd10fd4b1588f635e1738144c2
hyun3936 <hyun3936@naver.com>
2023-11-11 13:12:14 +0900

    11/11

--- edu/src/Chap_14/객체배열정렬comparator.java
+++ edu/src/Chap_14/객체배열정렬comparator.java
@@ -1,63 +1,96 @@
 package Chap_14;
 
-/*
- * public interface Comparator<T>{
- *    int compare(T 01, T 02);
- *    }
- *    
- * public interface Comparable<T> {
- *   public int compareTo(T o);
- *   }   
- */
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Random;
+
 class Student {
 	String sno;
 	String sname;
+
 	public Student(String sno, String sname) {
-		this.sno = sno; this.sname = sname;
+		this.sno = sno;
+		this.sname = sname;
 	}
+
 	public String toString() {
-		return "[" + sno + ", " + sname+"]";
+		return "[" + sno + ", " + sname + "]";
 	}
 
 }
-public class 객체배열정렬comparator {
-	static void sortStudent(Student[]data, Comparator<Student> comp) {
-		Student temp;
-		for (int i = 0; i < data.length;i++) 
-			for (int j = i + 1; j < data.length; j++)
-			{
 
+public class 객체배열정렬comparator {
+	static void sortStudent(Student[] data, Comparator<Student> comp) {
+		Student temp;
+		for (int i = 0; i < data.length; i++)
+			for (int j = i + 1; j < data.length; j++) {
+				if (comp.compare(data[i], data[j]) > 0) {
+					// 잘못된 순서에 있는 경우 요소를 교환합니다.
+					temp = data[i];
+					data[i] = data[j];
+					data[j] = temp;
+				}
 			}
 	}
-	
+
 	public static void main(String[] args) {
-		Comparator<Student> comp = new Comparator<Student>(){
+
+		Comparator<Student> compNo = new Comparator<Student>(){
 			@Override
 			public int compare(Student s1, Student s2) {
+				return Integer.parseInt(s1.sno) - Integer.parseInt(s2.sno); 
+				//Integer.parseInt()를 사용하여 학번을 정수로 변환한 다음, 두 값을 빼내어 순서를 결정합니다.
+	}
+	};
+		// sno를 사용한 비교
+		// 위에 식을 간단하게 람다식으로 표현
+		//Comparator<Student> compNo = (s1, s2) -> s1.sno.compareTo(s2.sno);
 
-		};
-		Student [] data = {
-				new Student("12", "홍길동"),
-				new Student("121", "홍길순"),
-				new Student("213", "홍길춘"),
-				new Student("9", "홍길홍")
-		};
-		sortStudent(data, comp);
-		//Arrays.sort(data, comp);//이게 문제가 있어서 sortStudent()를 구현
-		//Collections.sort(data, comp);
-		/*
-		Arrays.sort(data, new Comparator<Student>() {
+		Comparator<Student> compName = new Comparator<Student>(){
 			@Override
 			public int compare(Student s1, Student s2) {
+				// sname을 사용한 비교
+				return s1.sname.compareTo(s2.sname);
+			}
+			};
 
-		});
-		*/
-		for (Student st: data)
+		//Comparator<Student> compName = (s1, s2) -> s1.sname.compareTo(s2.sname);
+
+		Student[] data = {
+				new Student("12", "가길동"), 
+				new Student("121", "나길순"),
+				new Student("13", "다길춘"),
+				new Student("9", "라길홍") 
+				};
+		
+	//Comparator<Student> compNo = (s1, s2) -> s1.sname.compareTo(s2.sno);
+		
+		
+		ArrayList<Student> al = new ArrayList<>();
+		al.add(null);
+		
+		
+
+		
+		
+		
+		
+        sortStudent(data, compNo);
+		// 또는 sortStudent(data, compName)을 호출하여 이름으로 정렬할 수 있습니다.
+        
+
+//		Arrays.sort(data, compNo);//이게 문제가 있어서 sortStudent()를 구현
+		// Collections.sort(data, comp);
+		
+		
+//		  Arrays.sort(data, new Comparator<Student>() {
+//		  
+//		  @Override public int compare(Student s1, Student s2) {
+//		  
+//		  });
+		 
+		 
+		for (Student st : data)
 			System.out.print(" " + st);
 	}
+	}
 
-}
